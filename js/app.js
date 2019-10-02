a2b3781a94577e10a23c4ae5544c6a6522dc7e21
Lyaysan35 <lyaysanglick@gmail.com>
2019-10-01 22:17:28 -0600

    tamogotchi born

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// //class Computer {
+// 	constructor(brand, year, memory){
+// 		this.brand = brand;
+// 		this.year = year;
+// 		this.memory = memory;
+
+// 	}
+// 	account(){
+// 		console.log("one,two")
+// 	}
+
+//Create class Tamogotchi
+class Tamogotchi {
+	constructor(name, age){
+	    this.name = name;
+	    this.age = age;
+	    this.hunger = 10;
+	    this.sleepiness = 10;
+	    this.boredom = 10;
+	    const div = document.createElement('div');
+	    div.id = name;
+	    document.querySelector('body').appendChild(div);
+	    const image = document.createElement('img');
+	    image.src = "assets/bobic.jpg";
+	    image.className ='tomogachi';
+	    document.querySelector(`#${name}`).appendChild(image);
+	}
+	sing(){
+	 	console.log("bla bla bla");
+	}
+	displayHunger() {
+		document.querySelector('#hunger').innerHTML += (": " + this.hunger);
+	}
+}
+const bobick = new Tamogotchi("bobick", 5);
+bobick.displayHunger();
+bobick.sing();
+
+// $('#hunger').on('click',(e) => {
+// 	console.log(e.target)
+// });
+
+
+
+
+
